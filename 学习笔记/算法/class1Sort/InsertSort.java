9f3016a002cddb2a4228e45d590f919a2f0192df
StanLee0314 <38393573+StanLee0314@users.noreply.github.com>
2021-02-24 15:15:47 +0800

    Add files via upload

---
+++ 学习笔记/算法/class1Sort/InsertSort.java
@@ -0,0 +1,31 @@
+package class1Sort;
+
+//插入排序
+public class InsertSort {
+    public static int[] sortMethod(int[] a ){
+        if (a ==null || a.length < 2){
+            return a;
+        }
+        for(int start = 1; start < a.length ;start ++) {
+            for (int num  = start-1; num >= 0 && a[num] > a[num+1];num --){
+                    swap(a,num,num +1);
+            }
+        }
+        return a;
+    }
+    public  static int[] swap(int[] a,int i , int  j ) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        return a;
+
+    }
+    public static void main(String[] args) {
+        int [] a= {1,2,333,4,5,63,32,2,34,4};
+        int[] b = sortMethod(a);
+        for (int num:b){
+            System.out.print(num + ",");
+        }
+    }
+
+}
---
+++ 学习笔记/算法/class1Sort/SelectionSort.java
@@ -0,0 +1,33 @@
+package class1Sort;
+
+//选择排序
+public class SelectionSort {
+    public  static int[] swap(int[] a,int i , int  j ){
+        int temp = a[i];
+         a[i] = a[j];
+        a[j] = temp;
+   return  a;
+    }
+
+    public static int[] sortMethod(int[] a){
+        if(a == null || a.length <2 )
+            return a;
+        for ( int start = 0 ; start < a.length ; start ++){
+            int minIndex = start;
+            for (int j = start ; j < a.length ; j++ ){
+                minIndex = a[minIndex]<a[j]? minIndex:j;
+            }
+            swap(a, minIndex, start);
+        }
+
+        return a;
+    }
+    public static void main(String[] args) {
+        int [] a= {1,2,3,334,5,63,32,2,34,4};
+        int[] b = sortMethod(a);
+        for (int num:b){
+            System.out.print(num + ",");
+        }
+    }
+    }
+
